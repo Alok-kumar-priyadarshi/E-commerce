7f9d5116f44cdbaa4998db3ab63d64a44073d422
alok kumar priyadarshi <155714900+Alok-kumar-priyadarshi@users.noreply.github.com>
2026-02-11 02:20:00 +0530

    Update axios baseURL to use environment variable

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
+import.meta.env.VITE_API_URL
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/v1",
+    baseURL: import.meta.env.VITE_API_URL,
 })
 
 // REQUEST INTERCEPTOR
